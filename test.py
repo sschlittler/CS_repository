74a753946239e8d4b0cbd1d0e847c46bc88134f1
sschlittler <sana.schlittler@student.unisg.ch>
2025-03-12 18:22:32 +0100

    new

--- test.py
+++ test.py
@@ -1,9 +1,15 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
 st.title("Hallo, Streamlit!")
 st.write("Das ist meine erste Streamlit-App.")
 
-st.header("This is a new header")
+st.header("This is a header with a divider", divider="gray")
+
+chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
+
+st.area_chart(chart_data)
 
 
 
